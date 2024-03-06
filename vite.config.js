d08f834a9e27dbd6cc1cc904c3785110ca155bf9
zaid <zaid81567@gamil.com>
2024-03-06 08:22:33 +0530

    resolving build path issue encountered while deploying

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //specifying the output directory for build file
+  build: {
+    outDir: "build",
+  },
+});
